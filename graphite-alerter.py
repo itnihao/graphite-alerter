11df6a803d9f4c01b27f7fa687659364778acffd
huoxy <sykp241095@gmail.com>
2013-06-09 15:46:43 +0800

    add graphite-alerter.py

---
+++ graphite-alerter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from utils import load_metrics, load_plugins, logging
+
+metrics = []
+plugins = []
+
+def main():
+
+    global metrics
+    metrics = load_metrics()
+
+    global plugins
+    plugins = load_plugins()
+
+
+main()
