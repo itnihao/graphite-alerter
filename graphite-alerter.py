95ddec3dbd4cc2b6257460230d2568d31160f21d
huoxy <sykp241095@gmail.com>
2013-06-10 09:51:43 +0800

    leave only one check thread left

--- graphite-alerter.py
+++ graphite-alerter.py
@@ -3,6 +3,7 @@
 import time
 import threading
 from threading import Thread
+from collections import deque
 from utils import load_metrics, load_plugins, logging, do
 
 import schedule
@@ -16,23 +17,28 @@ def fetch():
         for target in plugin.targets:
             for metric in target.metrics:
                 metric.curr = metric.value
-    logging.info('refreshing metrics...')
+    logging.info('fetching metrics...')
 
 
-def check(plugin):
+def check():
 
     while True:
-        for target in plugin.targets:
-            for metric in target.metrics:
-#                logging.info('[%s] [%s]' % (threading.current_thread().name, metric.name))
-                if target.min <= metric.curr <= target.max:
-                    metric.retry = 0
-                else:
-                    metric.retry += 1
-                if metric.retry == 3:
-                    do(plugin, target, metric)
-                    metric.retry = 0 # re-schedule
-        time.sleep(5)
+        logging.info('checking metrics...')
+        for plugin in plugins:
+            for target in plugin.targets:
+                for metric in target.metrics:
+    #                logging.info('[%s] [%s]' % (threading.current_thread().name, metric.name))
+                    if target.min <= metric.curr <= target.max:
+                        metric.retry = 0
+                    else:
+                        metric.retry += 1
+                    if metric.retry == 3:
+                        metric.retry = 0 # re-schedule
+        time.sleep(20) # check interval
+
+def alert():
+
+    for plugin in plugins:
 
 
 def main():
@@ -43,15 +49,20 @@ def main():
     global plugins
     plugins = load_plugins(metrics)
 
-    fetch() # fetch first
+    fetch() # fetch once
 
-    for plugin in plugins:
-        t = Thread(target = check, args = (plugin, ))
-        t.setDaemon(True)
-        t.start()
+    # start fetch
+    t = Thread(target = fetch)
+    t.setDaemon(True)
+    t.start()
+
+    # start check
+    t = Thread(target = check)
+    t.setDaemon(True)
+    t.start()
 
     # always infinitely
-    schedule.every(10).seconds.do(fetch)
+    schedule.every(10).seconds.do(alert)
     while True:
         schedule.run_pending()
         time.sleep(1)
